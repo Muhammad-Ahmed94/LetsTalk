97eb95866880af3a9d8299557e3c9b34066349aa
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-05-06 00:49:17 +0500

    Add and update:  - Add base middleware function to backend developement server  - update package.json

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,28 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
+// Middle-ware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.json());
+app.use(cookieParser());
+
+if(process.env.NOCE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+};
 
 app.use((req, res) => {
     res.send("hello backend")
