6188fdc5385b49886e6a27747dcebc2dcf6df349
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-05-07 20:50:24 +0500

    refactor: add comments and format

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { redis } from "../lib/redis.js";
 import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
+// Creates access and refresh tokens for user with unique userid
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.ACCESSTOKEN_SECRET, {
     expiresIn: "5m",
@@ -13,19 +14,20 @@ const generateTokens = (userId) => {
   return { accessToken, refreshToken };
 };
 
+// Sets new http only, access and refresh cookie with respective tokens
 const setCookies = (res, accessToken, refreshToken) => {
-    res.cookie("Aceess_Cookie", accessToken, {
-        httpOnly: true,
-        maxAge: 5 * 60 * 1000,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production"
-    });
-    res.cookie("Refresh_Cookie", refreshToken, {
-      httpOnly: true,
-      maxAge: 10 * 60 * 1000,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production"
-    });
+  res.cookie("Aceess_Cookie", accessToken, {
+    httpOnly: true,
+    maxAge: 5 * 60 * 1000,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.cookie("Refresh_Cookie", refreshToken, {
+    httpOnly: true,
+    maxAge: 10 * 60 * 1000,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  });
 };
 
 export const signup = async (req, res) => {
@@ -38,23 +40,22 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    // Create new user in DB
     const user = await userModel.create({ name, email, password });
-
     const { accessToken, refreshToken } = generateTokens(user._id);
 
     await redis.set(`refresh_Token:${user._id}`, refreshToken, "EX", 10 * 60);
-
     setCookies(res, accessToken, refreshToken);
 
+    // Sending new user in response back
     res.status(201).json({
-        user: {
-            _id: user._id,
-            name: user.name,
-            email: user.email
-        },
-        message: "New user created successfully"
-    })
-
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      message: "New user created successfully",
+    });
   } catch (error) {
     console.error("Signup error", error.message);
     res.sendStatus(500);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
-import path from 'path';
+import cors from "cors";
+import path from "path";
 
-import authRoutes from './routes/auth.route.js';
+import authRoutes from "./routes/auth.route.js";
 import connectDB from "./lib/db.js";
 
 dotenv.config();
@@ -15,22 +15,22 @@ const PORT = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
 // Middle-ware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
 
-if(process.env.NOCE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+if (process.env.NOCE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
-};
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
-    console.log(`Server running on port:${PORT}`);
-    connectDB();
-})
+  console.log(`Server running on port:${PORT}`);
+  connectDB();
+});
