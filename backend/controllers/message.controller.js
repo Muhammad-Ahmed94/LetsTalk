b0bc94a4b634fdb8c15cd02b83f31d81018e0cbc
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-05-11 23:27:19 +0500

    Add(func): Get message function to get all messages or 2 participants

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -7,10 +7,13 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params; // ID from params, receiver ID
         const senderId = req.user._id; // Id of logged in user
 
+        if(!message || !message.trim()) {
+            return res.status(400).json({ message: "Cannot send empty message" });
+        }
         // create conversation model for sender and receiver
         // between the current active participants
         let conversation = await conversationModel.findOne({
-            participants: { $all: [receiverId, senderId]}
+            participants: { $all: [senderId, receiverId]}
         })
 
         // if there is no conversation between the 2 clients before
@@ -31,13 +34,31 @@ export const sendMessage = async (req, res) => {
         if(newMessages) {
             conversation.messages.push(newMessages._id);
             await conversation.save();
-        };
+        }
 
         //TODO Socket.IO functionality will come here.
 
         res.status(201).json(newMessages);
+        console.log("Message sent:", newMessages.message);
     } catch (error) {
         console.error("Message not sent. Error in send message controller", error.message);
+        res.sendStatus(500);
+    }
+};
+
+export const getMessage = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await conversationModel.findOne({
+            participants: { $all: [senderId, receiverId]}
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+        console.log("Get message succcess");
+    } catch (error) {
+        console.error("Error in get messages controller", error.message);
         res.sendStatus(500);
     }
 }
