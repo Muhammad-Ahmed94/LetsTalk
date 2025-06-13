0648960c4bd57a49cab0f7ad6b64974672bba01c
Muhammad-Ahmed94 <muneebbum757@gmail.com>
2025-06-13 17:37:10 +0500

    final fixed version 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from "react-router";
 import { Toaster } from 'react-hot-toast';
 
-import Login from "./pages/LoginPage";
-import Home from "./pages/HomePage";
 import SignupPage from "./pages/Signup";
 import useUserStore from "./stores/useUserStore";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   const { user } = useUserStore();
@@ -12,11 +12,12 @@ const App = () => {
   return (
     <div className="p-4 min-h-screen flex-center">
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={user ? <HomePage /> : <Navigate to="/login" />} />
 
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          {/* <Route path="/login" element={user ? <Login /> : <Navigate to="/" />} /> */}
+          <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
 
-          <Route path="/signup" element={!user ? <SignupPage /> : <Navigate to="/" />} />
+          <Route path="/signup" element={!user ? <SignupPage/> : <Navigate to="/" />}/>
         </Routes>
         <Toaster  />
     </div>
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInst = axios.create({
-    baseURL: import.meta.env.DEV ? "http://localhost:5000/api" : "api",
-    withCredentials: true
+    baseURL: import.meta.env.DEV ? "http://localhost:5000/api" : "/api",
+    withCredentials: true,
+    timeout: 10000,
 })
 
 export default axiosInst;
