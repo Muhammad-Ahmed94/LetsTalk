04babc054838992f41f6c362638c1fd3defa7150
Muhammad-Ahmed94 <muneebbum757@gmail.com>
2025-06-13 17:00:50 +0500

    update: better error namings

--- backend/middleware/message.middleware.js
+++ backend/middleware/message.middleware.js
@@ -12,21 +12,24 @@ export const protectRoute = async (req, res, next) => {
         // If decoded, means token is still valid and secure
         // If not decoded, means access token has been compromised
       const decoded = jwt.verify(accessToken, process.env.ACCESSTOKEN_SECRET); // working
-      if(!decoded) { return res.status(401).json({ message: "User data compromised" }) };
+      if(!decoded) { return res.status(401).json({ message: "Invalid token" }) };
       
       const user = await userModel.findById(decoded.userId).select("-password");
       console.log("user", user.name);
 
-      if (!user) { return res.status(401).json({ message: "This user is unathuorized or been tempered with"}) }
+      if (!user) { return res.status(401).json({ message: "User not found"}) }
 
       req.user = user; // working
+      next();
     } catch (error) {
-      if (error.name === "TokenExpiredError") { return res.status(401).json({ message: "Token expired error" }) } 
+      if (error.name === "TokenExpiredError") { return res.status(401).json({ message: "Access token expired" }) };
+      if (error.name === "JsonWebTokenError") { return res.status(401).json({ message: "invalid token format" }) };
+
+      return res.status(401).json({ message: "Token verification failed" });
     }
 
-    next();
   } catch (error) {
-    console.error(`Unauthorized - Access denied`);
-    res.status(401).json({ message: "Unauthorized" });
+    console.error("Error in protectRoute middleware", error.message);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
