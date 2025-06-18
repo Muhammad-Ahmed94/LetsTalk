fa3c4badd57dd2d094163dfa685b1567026849bf
Muhammad-Ahmed94 <muneebbum757@gmail.com>
2025-06-18 16:50:50 +0500

    update: update socketio config

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,17 +7,37 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5000"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected socketID", socket.id);
+// Store user socket maping for real-time features
+const userSocketMap = {};
 
-  // socket.on() is used to listen to events on both client and server side.
+// Get receiver socket id
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+}
+
+io.on("connection", (socket) => {
+  console.log("User connected socketid:", socket.id);
+
+  // Get userId from socket handshake query and update usersocketmap
+  const userId = socket.handshake.query.userId;
+  if(userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+  };
+
+  // Generate emit event to update the online users list to all clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Disconnect user event
   socket.on("disconnect", () => {
-    console.log("User disconnected socket id", socket.id);
-  });
+    console.log("User disconnected socketId:", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  })
 });
+
 export { app, server, io };
